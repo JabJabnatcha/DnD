2200c6984930eff35472ccd3d3ef5aa721fc0ea3
JabJab Natcha <natcharapaphon@gmail.com>
2025-11-13 16:02:39 +0700

    reset

--- DndBackend/Layer_Domain/Items/Equipment.cs
+++
@@ -1,8 +0,0 @@
-namespace Layer_Domain.Entities.Items
-{
-    public class Equipment : ITEM
-    {
-        public int Durability { get; set; }
-        public bool IsEquipped { get; set; }
-    }
-}
--- DndBackend/Layer_Domain/Items/Tool.cs
+++
@@ -1,8 +0,0 @@
-namespace Layer_Domain.Entities.Items
-{
-    public class Tool : ITEM
-    {
-        public string ToolType { get; set; } = string.Empty;   // Rope, ThievesTools, MusicalInstrument, Artisan
-        public string? SpecialFeature { get; set; }            // e.g., Advantage on checks
-    }
-}
