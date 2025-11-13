fd7dc2b4584563db1cde5794b72e5a6dfc25dbf0
JabJab Natcha <natcharapaphon@gmail.com>
2025-11-13 18:07:21 +0700

    save build

--- backend/GameDnD.API/Program.cs
+++ backend/GameDnD.API/Program.cs
@@ -1,5 +1,6 @@
 using GameDnD.API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
---
+++ frontend/vue-project/src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import CharactersView from '../views/CharactersView.vue'
+import CreateCharacterView from '../views/CreateCharacterView.vue'
+
+const routes = [
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/characters', name: 'characters', component: CharactersView },
+  { path: '/characters/create', name: 'createCharacter', component: CreateCharacterView },
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+export default router
